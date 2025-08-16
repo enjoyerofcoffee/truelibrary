797cfa23d1b3a6b01fbfa788630e0a00b42b03b8
silvabacc <bavernaz@hotmail.com>
2025-08-16 02:09:31 +0100

    fix styling

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -117,7 +117,7 @@ function PostPage() {
   );
 
   return (
-    <Container display="flex" size="xl" pt={"sm"}>
+    <Container size="xl" pt={"sm"}>
       <Group
         gap={12}
         className={classes.tags__wrapper}
